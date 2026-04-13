7334d6d91fa9dc0401be85dac92af4ce28593dd1
gabriotz <gmomenezes@gmail.com>
2026-04-13 14:01:31 -0300

    chore/ Faturamento Service setup

---
+++ faturamento/FaturamentoService/Program.cs
@@ -0,0 +1,31 @@
+using FaturamentoService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//  Swagger / OpenAPI
+builder.Services.AddOpenApi();
+
+//  Controllers
+builder.Services.AddControllers();
+
+//  DbContext (PostgreSQL)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
+var app = builder.Build();
+
+// Swagger só em dev
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
---
+++ faturamento/FaturamentoService/data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace FaturamentoService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
