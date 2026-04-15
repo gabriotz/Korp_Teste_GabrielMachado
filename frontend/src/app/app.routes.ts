91194f903c1100d02438eb691153a34dfcc0e953
gabriotz <gmomenezes@gmail.com>
2026-04-14 23:52:04 -0300

    feature(angularHttpSetup): add HttpClient and router configuration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'produtos', pathMatch: 'full' },
+  {
+    path: 'produtos',
+    loadComponent: () =>
+      import('./features/produtos/listagem/listagem').then(m => m.ListagemComponent)
+  },
+  {
+    path: 'produtos/novo',
+    loadComponent: () =>
+      import('./features/produtos/formulario/formulario').then(m => m.FormularioComponent)
+  }
+];
